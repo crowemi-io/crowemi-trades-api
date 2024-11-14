768ebb2f9933db20ee2303d7057376b8da46027d
Andy Crowe <crowemi@hotmail.com>
2024-11-13 16:08:34 -0800

    removed servicea accountrole

--- infra/iam.tf
+++ infra/iam.tf
@@ -3,9 +3,3 @@ resource "google_service_account" "this" {
   display_name = "srv_crowemi_trades"
   description  = "A service account for ${local.service}"
 }
-resource "google_cloud_run_service_iam_member" "this" {
-  location = google_cloud_run_v2_service.this.location
-  service  = google_cloud_run_v2_service.this.name
-  role     = "roles/run.invoker"
-  member   = "serviceAccount:${google_service_account.this.email}"
-}
