b9433bf0237235a77c0d54c716ba014a08c3753e
Andy Crowe <crowemi@hotmail.com>
2024-11-13 21:17:24 -0800

    add secret manages

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,15 +1,44 @@
 data "google_secret_manager_secret" "alpaca_api_key" {
   secret_id = "CROWEMI_TRADES_ALPACA_API_KEY"
 }
+resource "google_secret_manager_secret_iam_member" "alpaca_api_key" {
+  secret_id = data.google_secret_manager_secret.alpaca_api_key.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member     = "serviceAccount:${google_service_account.this.email}"
+}
+
 data "google_secret_manager_secret" "alpaca_api_secret_key" {
   secret_id = "CROWEMI_TRADES_ALPACA_API_SECRET_KEY"
 }
+resource "google_secret_manager_secret_iam_member" "alpaca_api_secret_key" {
+  secret_id = data.google_secret_manager_secret.alpaca_api_secret_key.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member     = "serviceAccount:${google_service_account.this.email}"
+}
+
 data "google_secret_manager_secret" "alpaca_url_base" {
   secret_id = "CROWEMI_TRADES_ALPACA_API_URL_BASE"
 }
+resource "google_secret_manager_secret_iam_member" "alpaca_url_base" {
+  secret_id = data.google_secret_manager_secret.alpaca_url_base.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member     = "serviceAccount:${google_service_account.this.email}"
+}
+
 data "google_secret_manager_secret" "alpaca_data_url_base" {
   secret_id = "CROWEMI_TRADES_ALPACA_DATA_API_URL_BASE"
 }
+resource "google_secret_manager_secret_iam_member" "alpaca_data_url_base" {
+  secret_id = data.google_secret_manager_secret.alpaca_data_url_base.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member     = "serviceAccount:${google_service_account.this.email}"
+}
+
 data "google_secret_manager_secret" "mongodb_uri" {
   secret_id = "CROWEMI_TRADES_MONGODB_URI"
+}
+resource "google_secret_manager_secret_iam_member" "alpaca_data_url_base" {
+  secret_id = data.google_secret_manager_secret.alpaca_data_url_base.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member     = "serviceAccount:${google_service_account.this.email}"
 }
