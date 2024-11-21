60a61beb2c627ecf6dda2e0c9a7008d8f26010a0
Andy Crowe <crowemi@hotmail.com>
2024-11-20 16:28:56 -0800

    remove mongodb provider

--- infra/providers.tf
+++ infra/providers.tf
@@ -3,7 +3,3 @@ provider "google" {
   project     = "crowemi-io-417402"
   region      = "us-west1"
 }
-provider "mongodbatlas" {
-  public_key = var.mongodbatlas_public_key
-  private_key  = var.mongodbatlas_private_key
-}
