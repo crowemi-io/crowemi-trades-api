0ea72262e19434c92a149ef2dcbb9d1d5c0e0a99
Andy Crowe <crowemi@hotmail.com>
2024-11-21 21:56:16 -0800

    set to upper

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,5 +1,5 @@
 locals {
-  secret_name = local.env == "paper" ? "CROWEMI_${local.service}_${local.env}" : "CROWEMI_TRADES_API"
+  secret_name = local.env == "paper" ? "CROWEMI_${local.service}_${upper(local.env)}" : "CROWEMI_TRADES_API"
 }
 
 data "google_secret_manager_secret" "this" {
