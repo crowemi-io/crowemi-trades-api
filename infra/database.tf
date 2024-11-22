1228533a50f52d6ba0fc3d3ad9638de0370307a1
Andy Crowe <crowemi@hotmail.com>
2024-11-21 16:22:01 -0800

    give service account perms + us-west-1

--- infra/database.tf
+++ infra/database.tf
@@ -1,6 +1,12 @@
 resource "google_firestore_database" "this" {
   project     = var.google_project_id
   name        = "${local.service}-${local.env}"
-  location_id = "nam5"
+  location_id = "us-west1"
   type        = "FIRESTORE_NATIVE"
+}
+
+resource "google_project_iam_member" "firestore_service_account_role" {
+  project = var.google_project_id
+  role    = "roles/datastore.user" 
+  member  = "serviceAccount:${google_service_account.this.email}"
 }
