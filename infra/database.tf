c21618dd0ddf6c1a6372b0d7af6c37882e2d29e5
Andy Crowe <crowemi@hotmail.com>
2024-11-21 16:01:04 -0800

    update for firestore

--- infra/database.tf
+++ infra/database.tf
@@ -1,8 +1,6 @@
-resource "mongodbatlas_serverless_instance" "this" {
-  project_id   = var.atlas_project_id
-  name         = "${local.service}-${local.env}"
-
-  provider_settings_backing_provider_name = "GCP"
-  provider_settings_provider_name = "SERVERLESS"
-  provider_settings_region_name = "us-east1"
+resource "google_firestore_database" "this" {
+  project     = var.google_project_id
+  name        = "${local.service}-${local.env}"
+  location_id = var.google_region
+  type        = "NATIVE"
 }
