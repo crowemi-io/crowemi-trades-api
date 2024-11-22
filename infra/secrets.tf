3fccaac5fc143c20193ffc613835b6681516ea4b
crowemi.eth <crowemi@hotmail.com>
2024-11-21 22:14:44 -0800

    feat: additional changes for env (#2)
    * update env as json
    
    * update to add paper env
    
    * set manual dispathc
    
    * removed quotes
    
    * quote tf var
    
    * remove mongodb provider
    
    * update env var
    
    * update names
    
    * update image name
    
    * update service account
    
    * add new workspace
    
    * add hcls
    
    * update config
    
    * updated config to leverage b64
    
    * minor updates
    
    * added database cluster
    
    * update to serverless
    
    * update regiod
    
    * region... again
    
    * trying us-east1
    
    * update for firestore
    
    * update firestore db
    
    * give service account perms + us-west-1
    
    * update region
    
    * remove firestore
    
    * updated env
    
    * add comment
    
    * update for prod
    
    * update secret
    
    * set to upper
    
    * update dater

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,5 +1,10 @@
+locals {
+  secret_name = local.env == "paper" ? "CROWEMI_TRADES_API_PAPER" : "CROWEMI_TRADES_API"
+}
+
 data "google_secret_manager_secret" "this" {
-  secret_id = "CROWEMI_TRADES_API_${upper(local.env)}"
+  secret_id = local.secret_name
+
 }
 resource "google_secret_manager_secret_iam_member" "this" {
   secret_id = data.google_secret_manager_secret.this.secret_id
