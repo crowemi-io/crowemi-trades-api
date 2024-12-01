186a29852231e9b83142814a2901acdeea15300c
Andy Crowe <crowemi@hotmail.com>
2024-11-30 20:33:52 -0800

    add url mapping

--- infra/main.tf
+++ infra/main.tf
@@ -13,6 +13,18 @@ resource "google_service_account" "this" {
   description  = "A service account for ${local.service} ${local.env}"
 }
 
+resource "google_cloud_run_domain_mapping" "this" {
+    location = "us-west1"
+    name     = "api.trades.crowemi.com"
+
+    metadata {
+        namespace = local.project
+    }
+
+    spec {
+        route_name = google_cloud_run_v2_service.this.name
+    }
+}
 
 resource "google_cloud_run_v2_service" "this" {
   name         = local.name
